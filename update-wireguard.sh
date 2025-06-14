9c84f9d7b849a29ef9bffb3a257030c63b9a28fa
laurentdenizotlink <76491383+laurentdenizotlink@users.noreply.github.com>
2025-06-14 19:14:10 +0200

    Erreur sur update

--- update-wireguard.sh
+++ update-wireguard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt upgrade -y
-apt updade -y
+apt update -y
 
 apt install wireguard -y
 wg setconf wg0  "$(wget -qLO - https://raw.githubusercontent.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/refs/heads/main/wg-test.conf)"
