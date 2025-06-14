b630c6bbd3433b0af5670cc05ea85310ef6ce878
laurentdenizotlink <76491383+laurentdenizotlink@users.noreply.github.com>
2025-06-14 19:07:59 +0200

    Fichier configuration test

--- update-wireguard.sh
+++ update-wireguard.sh
@@ -4,4 +4,4 @@ apt upgrade -y
 apt updade -y
 
 apt install wireguard -y
-wg setconf wg0  "$(wget -qLO - https://github.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/wg-test.conf)"
+wg setconf wg0  "$(wget -qLO - https://raw.githubusercontent.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/refs/heads/main/wg-test.conf)"
