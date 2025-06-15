8e571db2e6f903752c5b6d33bac682c8baf43997
laurentdenizotlink <76491383+laurentdenizotlink@users.noreply.github.com>
2025-06-15 10:55:49 +0200

    Beta avec telechargement et creation wg

--- update-wireguard.sh
+++ update-wireguard.sh
@@ -6,4 +6,5 @@ apt update -y
 apt install wireguard -y
 apt install resolvconf -y
 
-wg setconf wg0  "$(wget -qLO - https://raw.githubusercontent.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/refs/heads/main/wg-test.conf)"
+wget -O /etc/wireguard/wg1.conf https://raw.githubusercontent.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/refs/heads/main/wg-test.conf
+wg-quick up wg1
