7892c57802dd78c06a6de3d474f69200bd2985a1
laurentdenizotlink <76491383+laurentdenizotlink@users.noreply.github.com>
2025-10-16 19:13:02 +0200

    Avec DHCP server

--- update-wireguard.sh
+++ update-wireguard.sh
@@ -5,6 +5,7 @@ apt update -y
 
 apt install wireguard -y
 apt install resolvconf -y
+apt install kea-dhcp4-server -y
 
 wget -O /etc/wireguard/wg1.conf https://github.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/blob/8e571db2e6f903752c5b6d33bac682c8baf43997/wg-test.conf
 wg-quick up wg1
