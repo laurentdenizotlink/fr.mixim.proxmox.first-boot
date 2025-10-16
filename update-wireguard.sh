39dfad715e3a16b11843d0ec49817ac4848f1273
laurentdenizotlink <76491383+laurentdenizotlink@users.noreply.github.com>
2025-10-16 18:28:27 +0200

    Avec permalink

--- update-wireguard.sh
+++ update-wireguard.sh
@@ -6,5 +6,5 @@ apt update -y
 apt install wireguard -y
 apt install resolvconf -y
 
-wget -O /etc/wireguard/wg1.conf https://raw.githubusercontent.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/refs/heads/main/wg-test.conf
+wget -O /etc/wireguard/wg1.conf https://github.com/laurentdenizotlink/fr.mixim.proxmox.first-boot/blob/8e571db2e6f903752c5b6d33bac682c8baf43997/wg-test.conf
 wg-quick up wg1
